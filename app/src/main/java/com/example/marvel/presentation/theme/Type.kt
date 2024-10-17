b01aa8cd9ec6b6f81600e7198ce0de427fefd54b
Yulia Shevtsova <yulia.shevtsova2005@gmail.com>
2024-10-17 21:26:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/marvel/presentation/theme/Type.kt
@@ -0,0 +1,39 @@
+package com.example.marvel.presentation.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.marvel.R
+
+val mainFont: FontFamily =
+    FontFamily(Font(resId = R.font.akzidenzgroteskpro_regular))
+
+val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight.ExtraBold,
+        letterSpacing = 0.05.sp
+    ),
+    headlineMedium = TextStyle(
+        fontSize = 28.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight.ExtraBold,
+        letterSpacing = 0.05.sp
+    ),
+    titleLarge = TextStyle(
+        fontSize = 34.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight.ExtraBold,
+        letterSpacing = 0.05.sp
+    ),
+    titleMedium = TextStyle(
+        fontSize = 22.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.05.sp
+    )
+)
